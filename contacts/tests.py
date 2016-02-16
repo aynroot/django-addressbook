4f2b4a01a334117f51cd44896e23034bdb2b53a8
Valeria Chernenko <valeriia.chernenko@tum.de>
2016-02-16 23:52:55 +0100

    added some tests

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from .models import Contact
+
+
+class ContactTest(TestCase):
+    def test_representation(self):
+        contact = Contact(first_name='Valeria', last_name='Chernenko')
+        self.assertEqual(str(contact), 'Valeria Chernenko')
+
+
+class ContactsCRUDTest(TestCase):
+    def setUp(self):
+        contact = Contact(first_name='Valeria', last_name='Chernenko')
+        contact.save()
+        self.contacts = [contact]
+
+    def test_list_contacts(self):
+        response = self.client.get(reverse('list_contacts'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Valeria' in response.content)
+
+    def test_delete_contacts(self):
+        response = self.client.get('/delete/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(r'Valeria' not in response.content)
+
+    def test_edit_contact(self):
+        response = self.client.post('/edit/1', {'first_name': ''})
+        self.assertFormError(response, 'form', 'first_name', 'This field is required.')
+
+        response = self.client.post('/edit/1', {'first_name': 'Ozel', 'last_name': 'Christo',
+                                                'phone': '+4917635382129'})
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('list_contacts'))
+        self.assertTrue('Ozel' in response.content)
+
+    def test_create_new_contact(self):
+        response = self.client.get('/new', {'first_name': 'Ozel', 'last_name': 'Christo',
+                                            'phone': '+4917635382129'})
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse('Valeria' in response.content)
